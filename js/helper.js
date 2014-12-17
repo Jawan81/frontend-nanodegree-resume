b7fabcbbeb7c110e7ffce0584e7964bfbba14fa6
Jawan81 <deadzac@gmx.net>
2014-12-17 18:04:01 +0100

    Added top navigation menu. Changed styles.

--- js/helper.js
+++ js/helper.js
@@ -38,7 +38,7 @@ var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDates = '<div class="date-text yellowFont">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
